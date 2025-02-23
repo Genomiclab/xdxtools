61024da84a0e267f71bf684fc146655017872ca3
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2025-02-23 18:19:31 +0800

    fix htseq2matrix issues

--- inst/Rscripts/htseq2matrix.R
+++ inst/Rscripts/htseq2matrix.R
@@ -19,32 +19,77 @@ for (i in 1:length(htseqfiles)){
   sampleid <- stringr::str_remove(htseqfiles[i],pattern = postfix)
   sampleid <- stringr::str_remove(sampleid,pattern = paste0(htseq_dir,"/"))
   message(glue::glue("processing {sampleid}"))
-  temp_df <- readr::read_delim(htseqfiles[i], 
-                               delim = "\t", escape_double = FALSE, 
-                               col_names = FALSE, col_types = cols(X2 = col_number()), 
+  temp_df <- readr::read_delim(htseqfiles[i],
+                               delim = "\t", escape_double = FALSE,
+                               col_names = FALSE, col_types = cols(X2 = col_number()),
                                trim_ws = TRUE)
   colnames(temp_df) <- c("Geneid",sampleid)
   if (is.null(origin_matrix)){
     origin_matrix <- temp_df
   }else{
-    origin_matrix <- origin_matrix %>% 
+    origin_matrix <- origin_matrix %>%
       dplyr::left_join(temp_df,by = "Geneid")
   }
 }
 
 message("transforming Entrezeid to Symbol")
-Entrez_Gene_Id_db <- readRDS("inst/Entrez_Gene_Id_db.RDS")
-matrix_count <- origin_matrix %>%
-  dplyr::right_join(data.frame(Geneid = Entrez_Gene_Id_db$ENSEMBL,
-                               Gene = Entrez_Gene_Id_db$SYMBOL),
-                    by = "Geneid") %>%
-  dplyr::select(-Geneid) %>%
-  dplyr::group_by(Gene) %>%
-  dplyr::summarise(across(everything(), ~max(., na.rm = TRUE))) %>%
-  dplyr::mutate(row_sum = rowSums(.[, -which(names(.) == "Gene")], na.rm = TRUE)) %>%
-  dplyr::filter(row_sum != 0 & row_sum != -Inf & !is.na(row_sum)) %>%
-  dplyr::select(-row_sum) %>%
-  dplyr::relocate(Gene)
+if (postfix == "_human.txt"){
+  Entrez_Gene_Id_db <- readRDS("inst/Entrez_Gene_Id_db.RDS")
+  matrix_count <- origin_matrix %>%
+    dplyr::right_join(data.frame(Geneid = Entrez_Gene_Id_db$ENSEMBL,
+                                 Gene = Entrez_Gene_Id_db$SYMBOL),
+                      by = "Geneid") %>%
+    dplyr::select(-Geneid) %>%
+    {
+      temp <- .
+      library(data.table)
+      # 假设 matrix_count 是 data.frame 或 data.table
+      setDT(temp)  # 原地转换为 data.table（无需复制）
+      # 按 Gene 分组，对所有列（除 Gene）求 max 并忽略 NA
+      temp <- temp[
+        , lapply(.SD, max, na.rm = TRUE),
+        by = Gene,
+        .SDcols = -"Gene"  # .SDcols = 指定需处理的列（排除 Gene）
+      ]
+      as.data.frame(temp)
+    } %>%
+    dplyr::mutate(row_sum = rowSums(.[, -which(names(.) == "Gene")], na.rm = TRUE)) %>%
+    dplyr::filter(row_sum != 0 & row_sum != -Inf & !is.na(row_sum)) %>%
+    dplyr::select(-row_sum) %>%
+    dplyr::relocate(Gene)
+} else {
+  library(org.Mm.eg.db)
+  Entrez_Gene_Id_db <- select(
+    org.Mm.eg.db,
+    keys = keys(org.Mm.eg.db, keytype = "ENTREZID"),
+    keytype = "ENTREZID",
+    columns = c("SYMBOL", "GENENAME", "ENSEMBL", "UNIPROT")
+  )
+  matrix_count <- origin_matrix %>%
+    dplyr::right_join(data.frame(Geneid = Entrez_Gene_Id_db$UNIPROT,
+                                 Gene = Entrez_Gene_Id_db$SYMBOL),
+                      by = "Geneid") %>%
+    dplyr::select(-Geneid) %>%
+    {
+      temp <- .
+      library(data.table)
+      # 假设 matrix_count 是 data.frame 或 data.table
+      setDT(temp)  # 原地转换为 data.table（无需复制）
+      # 按 Gene 分组，对所有列（除 Gene）求 max 并忽略 NA
+      temp <- temp[
+        , lapply(.SD, max, na.rm = TRUE),
+        by = Gene,
+        .SDcols = -"Gene"  # .SDcols = 指定需处理的列（排除 Gene）
+      ]
+      as.data.frame(temp)
+    } %>%
+    dplyr::mutate(row_sum = rowSums(.[, -which(names(.) == "Gene")], na.rm = TRUE)) %>%
+    dplyr::filter(row_sum != 0 & row_sum != -Inf & !is.na(row_sum)) %>%
+    dplyr::select(-row_sum) %>%
+    dplyr::relocate(Gene)
+}
+
+
 
 write.table(matrix_count,
             file = paste0(output_dir,"/matrix_count.txt"),
@@ -52,16 +97,16 @@ write.table(matrix_count,
             quote = F)
 saveRDS(matrix_count,
         file = paste0(output_dir,"/matrix_count.RDS"))
-matrix_norm <- matrix_count %>% 
-  tibble::column_to_rownames("Gene") %>% 
-  as.matrix() %>% 
+matrix_norm <- matrix_count %>%
+  tibble::column_to_rownames("Gene") %>%
+  as.matrix() %>%
   {
     m <- .+1
     m <- log2(m)
     m
-  } %>% 
-  as.data.frame() %>% 
-  dplyr::mutate_if(~!is.numeric(.), as.numeric) %>% 
+  } %>%
+  as.data.frame() %>%
+  dplyr::mutate_if(~!is.numeric(.), as.numeric) %>%
   tibble::rownames_to_column("Gene")
 
 write.table(matrix_norm,
