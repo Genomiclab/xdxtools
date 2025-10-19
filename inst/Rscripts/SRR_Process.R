b05773212cd343bf198e0057ea95e9d8d8f53ef4
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2025-10-19 16:15:01 +0800

    set default C1 C2 parameters

--- inst/Rscripts/SRR_Process.R
+++ inst/Rscripts/SRR_Process.R
@@ -1,34 +1,49 @@
-nohup Rscript -e '
-wds <- c("/public3/home/scg9946/rna_pdxdata/SRP302257");
+Rscript -e '
+# 加载所需包
+library(stringr)
+library(dplyr)
+library(fs)
+library(glue)
+
+wds <- c("/public3/home/scg9946/SRP175361")
 for (wd in wds) {
-  if (!dir.exists(wd)) dir.create(wd, recursive=TRUE);
-  setwd(wd);
-  if (!dir.exists("out")) dir.create("out");
-  SRRs <- list.files(full.names=F);
-  SRRs = SRRs[!(SRRs == "out")];
+  if (!dir.exists(wd)) dir.create(wd, recursive = TRUE)
+  setwd(wd)
+  if (!dir.exists("out")) dir.create("out")
+
+  SRRs <- list.files(full.names = FALSE)
+  SRRs <- SRRs[grepl("SRR",SRRs)]
+  processed_SRRs <- list.files("out/", full.names = FALSE) %>%
+    str_remove(., "_1.fastq.gz") %>%
+    str_remove(., "_2.fastq.gz")
+
+  # 修正 setdiff 和过滤逻辑
+  need_SRRs <- setdiff(SRRs, processed_SRRs)  # 直接使用 SRRs 无需 basename
+  SRRs <- SRRs[SRRs %in% need_SRRs]  # 简化过滤
+
   for (i in seq_along(SRRs)) {
-    sra_id <- SRRs[i];
-    cmd <- glue::glue(
+    sra_id <- SRRs[i]
+    cmd <- glue(
       "srun -p amd_512 -N1 -n1 --cpus-per-task=10 --mem=100G \\
        conda run -n sratools parallel-fastq-dump \\
        --sra-id {sra_id} --threads 20 --outdir out/ --split-files --gzip"
-    );
-    message("\n>> Processing ", SRRs[i], " (", i, "/", length(SRRs), ")");
-    message(">> Command: ", cmd);
+    )
+    message("\n>> Processing ", sra_id, " (", i, "/", length(SRRs), ")")
+    message(">> Command: ", cmd)
     tryCatch({
-      system(cmd, intern=TRUE);
-      message(">> ", SRRs[i], " completed");
-    }, error=function(e) message("!! Error: ", e$message));
-  };
-  message(">> Preparing Fastq files");
-  files <- list.files(path = paste0(wd,"/out/"),
-                    full.names = T);
-  library(dplyr);
-  files_rename <- stringr::str_replace(files,"_1.fastq.gz","_R1.fastq.gz") %>%
-  stringr::str_replace(.,"_2.fastq.gz","_R2.fastq.gz");
-  for (i in 1:length(files)){
-    `fs::file_move(path = files[i],
-                new_path = files_rename[i])
+      system(cmd, intern = TRUE)
+      message(">> ", sra_id, " completed")
+    }, error = function(e) message("!! Error: ", e$message))
+  }
+
+  # 重命名文件
+  message(">> Preparing Fastq files")
+  files <- list.files(path = "out/", full.names = TRUE)
+  files_rename <- files %>%
+    str_replace("_1.fastq.gz", "_R1.fastq.gz") %>%
+    str_replace("_2.fastq.gz", "_R2.fastq.gz")
+  for (i in seq_along(files)) {
+    file_move(files[i], files_rename[i])
   }
 }
 ' > download3.log 2>&1 &
